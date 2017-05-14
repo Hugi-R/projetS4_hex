d7705abbd44e41f993160d70c8c0bfc4e5de3616
MisterPupuce <ajy.roussel@gmail.com>
2017-05-14 22:28:37 +0200

    sauvegarde.h final
    mise a jour des arguments de chargerPartie ainsi que des codes d'erreurs.

--- sauvegarde.h
+++ sauvegarde.h
@@ -6,7 +6,7 @@
  * @param [input] g pointeur vers la grille
  * @param [input] nomPartie est le dossier ou se trouvent les sauvegardes de la partie
  * @param [input] historique String contenant l'historique déjà formaté
- * @return retourne le numero de sauvegarde, ou un code erreur (-x)
+ * @return retourne le numero de sauvegarde, ou un code erreur
  * 	code d'erreur:	-1: historique incohérent avec la grille 
  * 			-2: erreur lors de l'ouverture de répertoire nomPartie
  * 			-3: erreur lors de l'ouverture du fichier de sauvegarde
@@ -16,11 +16,13 @@ int sauvegarderPartie(Grille g, const char *nomPartie, const char *historique);
 /*
  * @brief crée une grille selon la sauvegarde indiquée
  * @param [input] nomPartie est le dossier ou se trouvent les sauvegardes de la partie
- * @param [input] numeroSauvegarde 
  * @param [output] g le pointeur vers la grille initialisé
  * @param [output] historique le String contenant l'historique
  * @return 0 si tout c'est bien passé, code d'erreur sinon
+ * 	code d'erreur:	-1: erreur lors de l'ouverture du fichier de sauvegarde
+ * 			-2: erreur lors de la copie de l'historique
+ * 			-3: erreur de format de sauvegarde
  */
-int chargerPartie(const char *nomPartie, int numeroSauvegarde, Grille *g, char *historique);
+int chargerPartie(const char *nomPartie, Grille *g, char **historique);
 
 #endif
