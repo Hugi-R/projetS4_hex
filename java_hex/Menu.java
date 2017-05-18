9159e15eed115eb9819d5e6ca1409c3e6413e259
Unknown <ajy.roussel@gmail.com>
2017-05-18 18:18:24 +0200

    ajout de load
    pas encore testable, ce n'est que les prémices

--- java_hex/Menu.java
+++ java_hex/Menu.java
@@ -38,21 +38,39 @@ public class Menu {
 		sc.nextLine();
 	}
 	
+	private static printFnames(String sDir){
+		File[] faFiles = new File(sDir).listFiles();
+		for(File file: faFiles){
+			System.out.println(file.getAbsolutePath());
+			}
+			if(file.isDirectory()){
+				printFnames(file.getAbsolutePath());
+			}
+		}
+	}
+	
+	private String load(){
+		String ret = "l ";
+		System.out.println("Entrez le nom d'une partie sauvegardée:");
+		printFnames("./save");
+		return ret;
+	}
+	
 	private void regles(){
 		System.out.println("REGLES:\n");
-		System.out.println("Présentation du jeu de Hex\n");
+		System.out.println("Présentation du jeu de Hex:\n");
 		System.out.println("	Le jeu de Hex est un jeu de société combinatoire abstrait pour deux joueurs.\n"+
-				" Il se joue sur un tablier en forme de losange dont les cases sont hexagonales.\n"+
-				" Toutes les dimensions du côté du losange sont possibles, la plus traditionnelle est celle composée par 11 hexagones,\n"+
-				" mais on trouve aussi les valeurs 13 ou 19. L’un des inventeurs du jeu, John Nash, préconise un tablier de taille 14 × 14.");
+				"Il se joue sur un tablier en forme de losange dont les cases sont hexagonales.\n"+
+				"Toutes les dimensions du côté du losange sont possibles, la plus traditionnelle est celle composée par 11 hexagones,\n"+
+				"mais on trouve aussi les valeurs 13 ou 19. L’un des inventeurs du jeu, John Nash, préconise un tablier de taille 14 × 14.");
 		
-		System.out.println("\n\nBut du jeu\n");
+		System.out.println("\n\nBut du jeu:\n");
 		System.out.println("	Le but du jeu est, pour chaque joueur, de relier les deux bords opposés du plateau de jeu (noir: B ou blanc: W)\n"+
-		" avec un chemin continu de pions.");
+		"avec un chemin continu de pions.");
 		
-		System.out.println("\n\nLes coups\n");
+		System.out.println("\n\nLes coups:\n");
 		System.out.println("	Chaque joueur joue à tour de rôle en posant un pion sur une case libre\n"+
-		" n’importe où sur le plateau de jeu. Le premier qui crée un chemin contigu de pions gagne la partie.\n");
+		"n’importe où sur le plateau de jeu. Le premier qui crée un chemin contigu de pions gagne la partie.\n");
 
 		System.out.println("Appuyez sur Entrée pour revenir au menu précédent");
 		sc.nextLine();
@@ -71,14 +89,18 @@ public class Menu {
 		System.out.println("	l: (load) charger une partie");
 		System.out.println("	h: (help) afficher l'aide de jeu");
 		System.out.println("	r: afficher les règles du jeu de Hex");
+		if (this.estMenuSecondaire)
+			System.out.println("	f: fermer ce menu");
 	}
-	
+
 	public String menu(){
 		String str;
 		printMenu();
 		str = sc.nextLine();
 		while (!(str.equals("n") ||
-		(this.estMenuSecondaire && (str.equals("s") || str.equals("q"))) ||
+		(this.estMenuSecondaire && (str.equals("f") ||
+					    str.equals("s") || 
+					    str.equals("q"))) ||
 		str.equals("l") ||
 		str.equals("h") ||
 		str.equals("r"))){
@@ -90,6 +112,8 @@ public class Menu {
 			case "h": this.help();
 					break;
 			case "r": this.regles();
+					break;
+			case "l": //this.load();
 					break;
 			default	:return str;
 		}
