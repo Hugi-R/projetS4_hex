41de290efd8a33d15649c954deeab8f42ee35bea
DESKTOP-A917PJG\Hugo <hugo.rous@gmail.com>
2017-05-18 17:52:51 +0200

    menu principal au démarage
    Main.java :
     - Lors du lancement le menu principal s'affiche et attend une entrée utilisateur.

--- java_hex/Main.java
+++ java_hex/Main.java
@@ -2,15 +2,19 @@ package java_hex;
 import java_hex.partie.*;
 
 public class Main{
+	private static int tailleGrille;
+	private static String nomPartie;
+	
 	public static void main(String[] args){
-		int a = 1;
-		int tailleGrille = 2;
-		String nomPartie = "partie_test";
+		int a = 0;
+		tailleGrille = 2;
+		nomPartie = "partie_test";
 		Partie p = new Partie(nomPartie);
 		do{
 			switch(a){
 				case 0 :
-					//TODO appel menu principal
+					a = menuPrincipal();
+					break;
 				case 1 : 
 					p.getGrille().destroy();
 					p = new Partie(nomPartie);
@@ -25,9 +29,30 @@ public class Main{
 					a = 3;
 					break;
 			}
-			a = p.jouer();
+			if( a == 1 || a == 2 )
+				a = p.jouer();
 		}while(a != 3);
 
 	}
+	
+	private static int menuPrincipal(){
+		Menu m = new Menu(false);
+		String rep = m.menu();
+		String[] commande = rep.split(" ", 2);
+		switch(commande[0]){
+			case("n") : 
+				String[] com = commande[1].split(" ");
+				nomPartie = com[0];
+				tailleGrille = Integer.parseInt(com[1]);
+				return 1;
+			case("l") : 
+				nomPartie = commande[1];
+				return 2;
+			case("q") : return 3;
+			default : return 0;
+		}
+	}
+	
+	
 }
 
