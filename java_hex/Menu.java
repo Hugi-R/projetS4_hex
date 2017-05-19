437bd294f176cf046172d8388fffe83516393148
Unknown <ajy.roussel@gmail.com>
2017-05-19 23:27:31 +0200

    ajout numero de ligne et colonne dans grille

--- java_hex/Menu.java
+++ java_hex/Menu.java
@@ -38,7 +38,7 @@ public class Menu {
 		"	- {historique, h}					=> affiche l'historique complet de la partie\n"+
 		"	- {undo, u} nombre					=> défait les \"nombre\" derniers coups joués\n"+
 		"ex. u 2						=> revient de 2 coups en arrière\n"+
-		"   - qs => (quick save) sauvegarde rapide, utilise le nom de la partie pour sauvegarder\n");
+		"   	- qs 							=> (quick save) sauvegarde rapide, utilise le nom de la partie pour sauvegarder\n");
 
 		System.out.println("Appuyez sur Entrée pour revenir au menu précédent");
 		sc.nextLine();
