a1429c2d57dafa3132ea56d073547b4f95868bc4
Unknown <5goupilis@gmail.com>
2017-05-20 19:56:08 +0200

    ia_tools complète
    les methodes de ia_tools permettent de réaliser une ia

--- ia_tools.c
+++ ia_tools.c
@@ -25,24 +25,14 @@ struct s_grille{
   Groupe *groupesBLU;
   int nbGroupesBLU;
 };
-/** fonction interne qui calcul la distance
- *tant que la 1ere direction est bloqué on continue sur la 2e
- *@param numcase numero de la node de depart
- *@param g grille pour accès ;
- *@param dir1 la 1ere direction
- *@param dir2 la 2e direction
- *@param couleur couleur du joueur 
- */ 
-static int dist( Grille g , int numcase , int couleur , int dir1 , int dir2);
+
 
 int* cherchePont ( Grille g , int numCase , int *nbpont ,  int couleur){
   int *Tab = (int*) malloc (sizeof (int)); 
   *nbpont = 0;
   for ( int i = 0 ;  i <6 ; i++ ){
     if (g->Tab[numCase]->cote[i]->color == VID){
-      //printf("case %d ",g->Tab[numCase]->cote[i]->numero);
       if (g->Tab[numCase]->cote[i]->cote[(i+1)%6]->color == couleur && g->Tab[numCase]->cote[(i+1)%6]->color == VID){
-	//printf(" %d:%d et %d:%d vide \n",g->Tab[numCase]->cote[i]->cote[(i+1)%6]->numero,g->Tab[numCase]->cote[i]->cote[(i+1)%6]->color,g->Tab[numCase]->cote[(i+1)%6]->numero,g->Tab[numCase]->cote[(i+1)%6]->color);
 	*nbpont+=1;
 	Tab = (int*) realloc (Tab , sizeof(int*)* *nbpont);
 	Tab[*nbpont-1] = g->Tab[numCase]->cote[i]->cote[(i+1)%6]->numero ;
@@ -66,15 +56,23 @@ int* coupDejaJouer ( Grille g , int couleur,  int *nbCoup ){
 	nbG = g->nbGroupesBLU;
   }
   for ( int i = 0 ; i < nbG ; i++ ){
-	  Tab = (int*) realloc (Tab , sizeof(int*)* (*nbCoup + tabGroupe[i]->size));
 	  for ( int j = 0 ; j < tabGroupe[i]->size ; j++){
-		  Tab[*nbCoup] = tabGroupe[i]->tab[j]->numero;
-		  *nbCoup+=1;
+		  if (tabGroupe[i]->tab[j]->numero >0 ){
+			 *nbCoup = *nbCoup + 1;
+			Tab = (int*) realloc (Tab , sizeof(int*)* (*nbCoup ));
+			Tab[*nbCoup-1] = tabGroupe[i]->tab[j]->numero;   
+			
+		  }
 	  }
   }
   return Tab;
 }
-
+/** @brief fonction interne qui recherche si un entier et dans le tableau d'entier donné en paramètre
+ * @param Tab le tableau d'entier a analyser
+ * @param Element entier que l'on va chercher dans le tableau
+ * @param nbElement nombre d'entier dans le tableau
+ * @return true si trouvé
+ */
 static bool appartient ( int *Tab , int Element , int nbElement){
 	bool estDansTab = true ;
 	for ( int i  = 0; i < nbElement && estDansTab ; i++){
@@ -85,22 +83,24 @@ static bool appartient ( int *Tab , int Element , int nbElement){
 	
 int* coupPossible ( Grille g , int couleur, int *nbcoup ){
   int pionDejaPoser ;
-  int *tabRecup;
-  int n ;
+  int *tabPont;
+  int nbPont ;
   int *TabCoup = (int*) malloc (sizeof (int)); 
   int * TabPionPose = coupDejaJouer(g,couleur,&pionDejaPoser);
   if ( pionDejaPoser == 0){
-	  TabCoup = (int*) realloc (TabCoup , sizeof(int*)* (g->size));
-	  *nbcoup = g->size ;
-	  for( int i = 0 ;  i < g->size ; i++ ){
+	  *nbcoup = 1 ;
 		  if ( couleur == RED ){
-			  TabCoup[i]= i + g->size;
-			  printf(" %d \n",i + g->size);
+			  if ( coupValide(g,g->size-2,1))
+				TabCoup[0]=(g->size-1)*(g->size-1);
+			  else
+				TabCoup[0]=(g->size-1)*(g->size);
 		  }else{
-			  TabCoup[i] = i*g->size +1;
-			  printf(" %d \n",i + g->size);
+			  if (coupValide(g,1,g->size-2))
+				TabCoup[0] = g->size + g->size-2 ;
+			  else
+				 TabCoup[0] = g->size-1;
 		  }
-	  }
+	  
   }else{
 	  *nbcoup =0 ;
 	  for ( int i = 0 ; i< pionDejaPoser ; i++){
@@ -114,72 +114,141 @@ int* coupPossible ( Grille g , int couleur, int *nbcoup ){
 				}
 			}
 		  }
-		  tabRecup = cherchePont(g,g->Tab[TabPionPose[i]]->numero,&n,VID);
-		  for ( int i = 0; i < n ; i++ ){
-			  if (appartient( TabCoup,tabRecup[i] , *nbcoup)){
+		  tabPont = cherchePont(g,g->Tab[TabPionPose[i]]->numero,&nbPont,VID);
+		  for ( int i = 0; i < nbPont ; i++ ){
+			  if (appartient( TabCoup,tabPont[i] , *nbcoup)){
 				*nbcoup= *nbcoup+1;
 				TabCoup = (int*) realloc (TabCoup , sizeof(int*)* (*nbcoup));
-				TabCoup[*nbcoup-1]= tabRecup[i];
+				TabCoup[*nbcoup-1]= tabPont[i];
 			  }
 		  }
+		  free(tabPont);
 	  }
-	  
   }
+  free(TabPionPose);
   return TabCoup ;
 }
 
 
-
- int calDistance ( Grille g ,  int numcase, int couleur , int direction){
-	int cas = numcase ;
-	int dis =0;
-	int a ,b;
-	while(g->Tab[cas]->cote[direction]->color == couleur || g->Tab[cas]->cote[direction]->color == VID){
-		cas = g->Tab[cas]->cote[direction]->numero;
-		dis++;
+int Cal_Distance (Grille g ,  int numCase, int couleur , int direction){
+	if ( g->Tab[numCase]->cote[direction]->color <0){
+		return 0 ;
 	}
-	if ( g->Tab[cas]->cote[direction]->color >-1){
-		a = dist( g ,cas , couleur , direction,(6+direction+1)%6 );
-		b = dist( g ,cas , couleur , direction,(6+direction-1)%6 );
-		if ( a>b)
-			return b+dis;
-		else 
-			return a+dis ;
-	}else{
-		return dis;
-	}
-		
- }
-
-static int dist( Grille g , int numcase , int couleur , int dir1 , int dir2){
-	int cas = numcase ;
-	int dis =0;
-	bool conti = true;
-	while(( g->Tab[cas]->cote[dir1]->color != couleur && g->Tab[cas]->cote[dir1]->color != VID)&& conti){
-		if (g->Tab[cas]->cote[dir2]->color == VID){
-			cas = g->Tab[cas]->cote[dir2]->numero;
-			dis++;
-		}else if ( g->Tab[cas]->cote[dir2]->color == couleur ){
-			cas = g->Tab[cas]->cote[dir2]->numero;
-		}else{
-			conti = false ;
-		}
-	}
-	printf("dis = %d \n",dis);
-	if ( conti ){
-		return dis+calDistance(g,cas,couleur,dir1);
+	if ( g->Tab[numCase]->cote[(6+direction+1)%6]->color == couleur ){
+		return Cal_Distance(g,g->Tab[numCase]->cote[(6+direction+1)%6]->numero,couleur,direction);
+	}else if ( g->Tab[numCase]->cote[(direction)]->color == couleur){
+		return Cal_Distance(g,g->Tab[numCase]->cote[(direction)]->numero,couleur,direction);
+	}else if (g->Tab[numCase]->cote[(6+direction-1)%6]->color == couleur){
+		return Cal_Distance(g,g->Tab[numCase]->cote[(6+direction-1)%6]->numero,couleur,direction);
+	}else if (g->Tab[numCase]->cote[(direction)]->color == VID ){
+		return Cal_Distance(g,g->Tab[numCase]->cote[direction]->numero,couleur,direction) +1;
+	}else if (g->Tab[numCase]->cote[(6+direction+1)%6]->color == VID ){
+		return Cal_Distance(g,g->Tab[numCase]->cote[(6+direction+1)%6]->numero,couleur,direction)+1;
+	}else if (g->Tab[numCase]->cote[(6+direction-1)%6]->color == VID ){
+		return Cal_Distance(g,g->Tab[numCase]->cote[(6+direction-1)%6]->numero,couleur,direction)+1;
 	}else{
 		return 1000;
 	}
 }
-
-void afficheTab ( int *Tab , int nb){
-	printf("valeur des %d cases tableau :",nb); 
-	for ( int i = 0 ; i < nb ; i ++){
-		printf(" %d ",Tab[i]);
+		
+	
+int pontEnDanger ( Grille g , int couleur,int *TabCoup, int nbcoup ){
+	int couleurAdverse ;
+	int coup;
+	bool Danger = false ;
+	if (couleur == RED)
+		couleurAdverse = BLU ;
+	else
+		couleurAdverse = RED ;
+	for( int i = 0 ; i< nbcoup && !Danger ; i++ ){
+		for ( int j=0 ; j<6 && !Danger; j++){
+			if(g->Tab[TabCoup[i]]->cote[j]->color == couleurAdverse){
+				if ( g->Tab[TabCoup[i]]->cote[j]->cote[(6+j-1)%6]->color == couleur && g->Tab[TabCoup[i]]->cote[(6+j-1)%6]->color == VID){
+					Danger = true ;
+					coup = g->Tab[TabCoup[i]]->cote[(6+j-1)%6]->numero;
+				}else if (g->Tab[TabCoup[i]]->cote[j]->cote[(6+j+1)%6]->color == couleur && g->Tab[TabCoup[i]]->cote[(6+j+1)%6]->color == VID){
+					Danger = true ;
+					coup = g->Tab[TabCoup[i]]->cote[(6+j+1)%6]->numero;
+				}
+			}
+		}
 	}
-	printf("\n");
+	if (!Danger)
+		coup = -1;
+	return coup ;
 }
+int minimax ( Grille g , int couleur ){
+	int *Tab;
+	int nbCoup ;
+	Node NodeAAnaliser;
+	int case_a_jouer =-1 ;
+	int a ,b ;
+	int pts_du_coup_a_jouer = -100000000;
+	bool pont;
+	int pts_du_coup ,direction1 , direction2 ;
+	int couleurAdverse ;
+	
+	
+	if (couleur == RED)
+		couleurAdverse = BLU ;
+	else
+		couleurAdverse = RED ;
+	Tab = coupDejaJouer(g,couleur,&nbCoup);
+	case_a_jouer = pontEnDanger(g,couleur,Tab,nbCoup);
+	if (case_a_jouer != -1){
+		free(Tab);
+		return case_a_jouer;
+	}
+	free(Tab);
+	Tab = coupPossible(g,couleur,&nbCoup);
+	
+	if ( couleur == RED ){
+		direction1 = 0 ;
+		direction2 = 3 ;
+	}else{
+		direction1 = 2 ;
+		direction2 = 5 ;
+	}
+	
+	for ( int i = 0 ; i < nbCoup ; i++){
+		pts_du_coup = 0 ; 
+		pont = true ;
+		for ( int j = 0 ; j < 6 ; j ++){
+			NodeAAnaliser = g->Tab[Tab[i]]->cote[j];
+			if ( NodeAAnaliser->color == couleur){
+				pont = false ;
+				pts_du_coup += 1;
+			}
+			if (NodeAAnaliser->color == couleurAdverse && (j == direction1 || j == direction2)  ){
+				pts_du_coup+= -2 ;
+			}
+			if (pont){
+				pts_du_coup+= 2;
+			}
+		}
+		a= Cal_Distance(g,g->Tab[Tab[i]]->numero,couleur ,direction1);
+		b = Cal_Distance(g,g->Tab[Tab[i]]->numero,couleur ,direction2);
+		pts_du_coup= pts_du_coup - ((a +b)*10*g->size);
+		if (pts_du_coup > pts_du_coup_a_jouer){
+			pts_du_coup_a_jouer = pts_du_coup;
+			case_a_jouer = g->Tab[Tab[i]]->numero;
+		}
+	}
+	free(Tab);
+	return case_a_jouer;
+}
+
+void convertisseur ( Grille g , int numcase , int *ligne , int *colonne ){
+	int cas = numcase ;
+	*ligne =0;
+	*colonne = 0 ;
+	while ( cas > g->size ){
+		*ligne = *ligne +1;
+		cas = cas- g->size;
+	}
+	*colonne = cas ;
+}
+
 		
 	  
 	  
